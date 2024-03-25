afb03ac862e3159fa3591495bf2ed42c44e04891
Aristide <dovi.aristide@gmail.com>
2024-03-25 22:28:24 +0100

    add seeder for user database

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,11 +2,14 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\User;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase; 
     /**
      * Test get all users
      */
